b11701f3356182d3c210dca490128a6578fa4fad
kmartinez834 <karinamartinez@gwu.edu>
2023-10-23 05:23:48 +0000

    changes to img augmentation and model architecture

--- test_Jeanne.py
+++ test_Jeanne.py
@@ -4,10 +4,14 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 import argparse
+import random
 from sklearn.metrics import f1_score, cohen_kappa_score, accuracy_score, matthews_corrcoef
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras import layers
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.initializers import glorot_uniform
+from tensorflow.keras.layers import Dense, Dropout, BatchNormalization, Activation
+from sklearn.utils.class_weight import compute_class_weight
 #------------------------------------------------------------------------------------------------------------------
 
 '''
@@ -15,6 +19,12 @@ from tensorflow.keras.optimizers import Adam
  02/14/2022 LSDR CHECK CONSISTENCY
 '''
 #------------------------------------------------------------------------------------------------------------------
+SEED = 123
+random.seed(SEED)
+np.random.seed(SEED)
+tf.random.set_seed(SEED)
+weight_init = glorot_uniform(seed=SEED)
+rng = tf.random.Generator.from_seed(SEED, alg='philox')
 
 CHANNELS = 3
 IMAGE_SIZE = 100
@@ -32,14 +42,21 @@ PATH = args.path
 DATA_DIR = args.path + os.path.sep + 'Data' + os.path.sep
 SPLIT = args.split
 
-n_epoch = 3
+
+'''PATH = '/home/ubuntu/Exam1-v4'
+DATA_DIR = PATH + os.path.sep + 'Data' + os.path.sep
+SPLIT = "test"'''
+
+n_epoch = 2
 BATCH_SIZE = 32
-LR = 0.2
+LR = 0.1
+DROPOUT = 0.2
+
 ## Process images in parallel
 AUTOTUNE = tf.data.AUTOTUNE
 
 NICKNAME = 'Jeanne'
-seed = (1,2)
+
 
 #------------------------------------------------------------------------------------------------------------------
 
@@ -100,18 +117,20 @@ def process_target(target_type):
 
 def augment(image):
 
-    global seed
+    #g = tf.random.Generator.from_non_deterministic_state()
     # Make a new seed.
-    seed = tf.random.split(seed, num=1)[0, :]
+    #img_seed = tf.random.split(seed, num=1)[0, :]
+    img_seed = rng.make_seeds(2)[0]
 
+    image = tf.image.resize( image, [IMAGE_SIZE, IMAGE_SIZE])
     # Random crop back to the original size.
-    #image = tf.image.stateless_random_crop(image, size=(IMAGE_SIZE, IMAGE_SIZE,CHANNELS), seed=seed)
+    image = tf.image.stateless_random_crop(image, size=(int(IMAGE_SIZE*.8),int(IMAGE_SIZE*.8),CHANNELS), seed=img_seed)
     # Random flip left right
-    image = tf.image.stateless_random_flip_left_right(image, seed=seed)
+    image = tf.image.stateless_random_flip_left_right(image, seed=img_seed)
     # Random saturation
-    image = tf.image.stateless_random_saturation(image, 0.1, 4.0, seed=seed)
+    image = tf.image.stateless_random_saturation(image, 0.1, 4.0, seed=img_seed)
     # Random flip up down
-    #image = tf.image.stateless_random_flip_left_right(image, seed=seed)
+    #image = tf.image.stateless_random_flip_up_down(image, seed=img_seed)
     return image
 
 #------------------------------------------------------------------------------------------------------------------
@@ -133,15 +152,17 @@ def process_path(feature, target):
    ## REad the image from disk
     img = tf.io.read_file(file_path)
     img = tf.io.decode_image(img, channels=CHANNELS, expand_animations=False)
-    
-    ## Resize the image
-
-    img = tf.image.resize_with_crop_or_pad( img, 300, 300)
-    img = tf.image.resize( img, [IMAGE_SIZE, IMAGE_SIZE])
 
     ## Randomly augment images
     if train == True:
         img = augment(img)
+
+    ## Resize the image
+    #img = tf.image.resize_with_crop_or_pad( img, 300, 300)
+    img = tf.image.resize( img, [IMAGE_SIZE, IMAGE_SIZE])
+
+    #plt.imshow(np.array(img, dtype=int))
+    #plt.show()
 
    ## Reshape the image to get the right dimensions for the initial input in the model
     img = tf.reshape(img, [-1])
@@ -178,8 +199,8 @@ def read_train_data(num_classes):
     global xdf_dset
 
     # Drop random sample of class5 rows
-    drop_rows = xdf_dset[xdf_dset['target']=='class5'].sample(frac=0.5, random_state=123).index
-    xdf_dset = xdf_dset.drop(drop_rows)
+    #drop_rows = xdf_dset[xdf_dset['target']=='class5'].sample(frac=0.5, random_state=SEED).index
+    #xdf_dset = xdf_dset.drop(drop_rows)
 
     max_class_count = (xdf_dset['target'].value_counts()).iloc[0]
     for name in class_names:
@@ -248,18 +269,41 @@ def model_definition():
 
     # Define the first dense layer
     model.add(tf.keras.layers.Dense(300, activation='relu', input_shape=(INPUTS_r,)))
+    #model.add(Dropout(DROPOUT, seed=SEED))
+    model.add(BatchNormalization())
     model.add(tf.keras.layers.Dense(200, activation='relu'))
+    #model.add(Dropout(DROPOUT, seed=SEED))
+    model.add(BatchNormalization())
     model.add(tf.keras.layers.Dense(100, activation='relu'))
+    model.add(Dropout(DROPOUT, seed=SEED))
+    model.add(BatchNormalization())
     model.add(tf.keras.layers.Dense(100, activation='relu'))
+    model.add(Dropout(DROPOUT, seed=SEED))
+    model.add(BatchNormalization())
     model.add(tf.keras.layers.Dense(80, activation='relu'))
+    model.add(Dropout(DROPOUT, seed=SEED))
+    model.add(BatchNormalization())
     model.add(tf.keras.layers.Dense(50, activation='relu'))
 
-    model.add(tf.keras.layers.Dense(OUTPUTS_a, activation='softmax')) #final layer , outputs_a is the number of targets
+    model.add(tf.keras.layers.Dense(OUTPUTS_a, activation='softmax', kernel_initializer=weight_init)) #final layer , outputs_a is the number of targets
 
-    model.compile(optimizer=Adam(learning_rate=LR), loss='categorical_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer=Adam(lr=LR), loss='categorical_crossentropy', metrics=['accuracy'])
 
     save_model(model) #print Summary
     return model
+
+#------------------------------------------------------------------------------------------------------------------
+# https://stackoverflow.com/questions/41648129/balancing-an-imbalanced-dataset-with-keras-image-generator
+def balance_model(class_names):
+
+    unique_classes = class_names
+    y = xdf_data['target'].to_numpy()
+
+    class_weights = compute_class_weight('balanced',classes=unique_classes, y=y)
+    #train_class_weights = {class_id: weight for class_id, weight in zip(unique_classes, class_weights)}
+    train_class_weights = dict(enumerate(class_weights))
+
+    return train_class_weights
 
 #------------------------------------------------------------------------------------------------------------------
 
@@ -268,12 +312,13 @@ def train_func(train_ds):
         train the model
     '''
 
-    #early_stop = tf.keras.callbacks.EarlyStopping(monitor='accuracy', patience =100)
+    early_stop = tf.keras.callbacks.EarlyStopping(monitor='accuracy', patience =100)
     check_point = tf.keras.callbacks.ModelCheckpoint('model_{}.h5'.format(NICKNAME), monitor='accuracy', save_best_only=True)
     final_model = model_definition()
 
-    #final_model.fit(train_ds,  epochs=n_epoch, callbacks=[early_stop, check_point])
-    final_model.fit(train_ds,  epochs=n_epoch, callbacks=[check_point])
+    final_model.fit(train_ds,  epochs=n_epoch, callbacks=[early_stop, check_point])
+    #final_model.fit(train_ds,  epochs=n_epoch, callbacks=[check_point])
+    #final_model.fit(train_ds,  epochs=n_epoch, callbacks=[check_point], class_weight=train_class_weights)
 
 #------------------------------------------------------------------------------------------------------------------
 
@@ -374,10 +419,24 @@ def metrics_func(metrics, aggregates=[]):
         print('Average of Metrics : ', xsum/xcont)
     # Ask for arguments for each metric
 
+    print("Accuracy breakdown by class:")
+    print("---------------------------")
+    label_accs = {}
+    for label in range(10):
+        label_ind = (y_true == label)
+        # extract predictions for specific true label
+        pred_label = y_pred[label_ind]
+        labels = y_true[label_ind]
+        # compute class-wise accuracy
+        label_accs[label] = accuracy_score(labels, pred_label)
+        #label_accs[accuracy_score(labels, pred_label)] = label 
+    for key,value in label_accs.items():
+        print(f"Class {key}: {value:.3f}")
+
 #------------------------------------------------------------------------------------------------------------------
 
 def main():
-    global xdf_data, class_names, INPUTS_r, OUTPUTS_a, xdf_dset, train, seed
+    global xdf_data, class_names, INPUTS_r, OUTPUTS_a, xdf_dset, train
 
     ## Reading the excel file from a directory
     for file in os.listdir(PATH+ os.path.sep+ "excel"):
@@ -398,6 +457,8 @@ def main():
     ## Processing Train dataset
 
     train = True
+    #train_class_weights = balance_model(class_names)
+
     xdf_dset = xdf_data[xdf_data["split"] == 'train'].copy()
 
     train_ds = read_train_data( OUTPUTS_a )
@@ -413,8 +474,8 @@ def main():
     predict_func(test_ds)
 
     ## Metrics Function over the result of the test dataset
-    list_of_metrics = ['f1_macro', 'coh']
-    list_of_agg = ['avg']
+    list_of_metrics = ['f1_macro','acc','coh']
+    list_of_agg = ['avg','sum']
     metrics_func(list_of_metrics, list_of_agg)
 
 if __name__ == "__main__":
